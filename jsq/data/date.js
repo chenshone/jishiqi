a73dc8bc1c73d9a8da33cd9bb9fa587e0454ffe4
chenshone <1351766353@qq.com>
2019-05-10 12:55:26 +0800

    完成列表页上部分

---
+++ jsq/data/date.js
@@ -0,0 +1,17 @@
+var dateList = [{
+	id:1,
+	date:"5月5日",
+	year:"2019",
+	week:"周日"
+},
+{
+	id:2,
+	date:"5月6日",
+	year:"2019",
+	week:"周一"
+}
+]
+
+module.exports = {
+	dateList:dateList
+}
--- jsq/pages/new/new.js
+++ jsq/pages/new/new.js
@@ -1,4 +1,5 @@
 // pages/new/new.js
+var dateObj = require("../../data/date.js");
 Page({
 
   /**
@@ -12,7 +13,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+	  this.setData({
+		  dateList:dateObj.dateList,
+	  })
   },
 
   /**
