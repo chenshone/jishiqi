d09ff26517c8782594808fd5194714eca08ce4c3
chenshone <1351766353@qq.com>
2019-05-12 21:45:42 +0800

    更新

--- jsq/data/date.js
+++ jsq/data/date.js
@@ -2,57 +2,80 @@ var dateList = [{
 	date:"5月5日",
 	year:"2019",
 	week:"周日",
-	athings:[{
+	things:[{
 		title:"打工1",
-		content:"按时大大娃娃发发发我方法的飞洒发"
-	}],
-	bthings:[{
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:1,
+		time:"00:00"
+	},
+	{
 		title:"打工2",
-		content:"按时大大娃娃发发发我方法的飞洒发"
-	}],
-	cthings:[{
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:2,
+		time:"00:00"
+	},
+	{
 		title:"打工3",
-		content:"按时大大娃娃发发发我方法的飞洒发"
-	}],
-	dthings:[{
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:3,
+		time:"00:00"
+	},
+	{
 		title:"打工4",
-		content:"按时大大娃娃发发发我方法的飞洒发"
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:4,
+		time:"00:00"
 	}]
-	
 },
 {
 	date:"5月6日",
 	year:"2019",
 	week:"周一",
-	athings:[{
+	things:[{
 		title:"打工5",
-		content:"按时大大娃娃发发发我方法的飞洒发"
-	}],
-	bthings:[{
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:1,
+		time:"00:00"
+	},
+	{
 		title:"打工6",
-		content:"按时大大娃娃发发发我方法的飞洒发"
-	}],
-	cthings:[{
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:2,
+		time:"00:00"
+	},
+	{
 		title:"打工7",
-		content:"按时大大娃娃发发发我方法的飞洒发"
-	}],
-	dthings:[{
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:3,
+		time:"00:00"
+	},
+	{
 		title:"打工8",
-		content:"按时大大娃娃发发发我方法的飞洒发"
+		content:"按时大大娃娃发发发我方法的飞洒发",
+		level:4,
+		time:"00:00"
 	}]
-	
 }
 ]
 
 var dateListNum = [];
 let j = 0;
+
+
 for(let i of dateList){
+	let a = 0,b = 0,c = 0,d = 0;
+	for(let k of i.things){
+		if(k.level === 1) a++;
+		if(k.level === 2) b++;
+		if(k.level === 3) c++;
+		if(k.level === 4) d++;
+	}
 	dateListNum[j] = {
-		allNum:i.athings.length + i.bthings.length + i.cthings.length + i.dthings.length,
-		aNum:i.athings.length,
-		bNum:i.bthings.length,
-		cNum:i.cthings.length,
-		dNum:i.dthings.length
+		allNum:i.things.length,
+		aNum:a,
+		bNum:b,
+		cNum:c,
+		dNum:d
 	}
 	j++;
 }
