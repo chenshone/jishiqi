59bf93c82b24833bb0ce3c7ae43f17f973b16049
chenshone <1351766353@qq.com>
2019-05-15 16:29:28 +0800

    日期轴完成50%

--- jsq/pages/list/list.js
+++ jsq/pages/list/list.js
@@ -2,71 +2,70 @@
 var dateObj = require("../../data/date.js");
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-	  this.setData({
-		  dateList:dateObj.dateList,
-		  len:dateObj.dateListLen,
-		  listNum:dateObj.dateListNum,
-		  listNumLen:dateObj.dateListNumLen
-	  })
-  },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function(options) {
+		this.setData({
+			dateList: dateObj.dateList,
+			len: dateObj.dateListLen,
+			listNum: dateObj.dateListNum,
+			listNumLen: dateObj.dateListNumLen
+		})
+	},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function() {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function() {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function() {
 
-  },
+	},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function() {
 
-  },
+	},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function() {
+	},
 
-  },
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+	},
 
-  },
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+	}
 })
