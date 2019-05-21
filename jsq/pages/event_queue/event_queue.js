8d1f58489579e26e448ff614f8cb40b5ecd41567
chenshone <1351766353@qq.com>
2019-05-22 02:13:48 +0800

    页面布局基本完成

--- jsq/pages/event_queue/event_queue.js
+++ jsq/pages/event_queue/event_queue.js
@@ -2,6 +2,13 @@
 //获取应用实例
 var util = require('../../utils/util.js');
 Page({
+	data:{
+		isTouch1:false,
+		isTouch2:false,
+		isTouch3:false,
+		isTouch4:false
+	},
+	
 	onLoad: function(options) {
 		let time = util.formatTime(new Date());
 		let date = util.getDates(time);
@@ -15,5 +22,63 @@ Page({
 			day:dateArr[2],
 			week:date[0].week
 		})
+	},
+	
+	isTouch1:function(){
+		if(this.data.isTouch1){
+			this.setData({
+				isTouch1:false
+			})
+		}else{
+			this.setData({
+				isTouch1:true,
+				isTouch2:false,
+				isTouch3:false,
+				isTouch4:false
+			})
+			
+		}
+	},
+	isTouch2:function(){
+		if(this.data.isTouch2){
+			this.setData({
+				isTouch2:false
+			})
+		}else{
+			this.setData({
+				isTouch2:true,
+				isTouch1:false,
+				isTouch3:false,
+				isTouch4:false
+			})
+		}
+	},
+	isTouch3:function(){
+		if(this.data.isTouch3){
+			this.setData({
+				isTouch3:false
+			})
+		}else{
+			this.setData({
+				isTouch3:true,
+				isTouch1:false,
+				isTouch2:false,
+				isTouch4:false
+			})
+		}
+	},
+	isTouch4:function(){
+		if(this.data.isTouch4){
+			this.setData({
+				isTouch4:false
+			})
+		}else{
+			this.setData({
+				isTouch4:true,
+				isTouch1:false,
+				isTouch2:false,
+				isTouch3:false
+			})
+		}
 	}
 })
